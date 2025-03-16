a48de445d59a3d0db21b1c42659830abcd8fd051
somb1 <somersbyappl@gmail.com>
2025-03-16 23:32:47 +0900

    edit scripts

--- scripts/post_start.sh
+++ scripts/post_start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export PYTHONUNBUFFERED=1
-
-cd /workspace
-mkdir -p /workspace/logs /workspace/models
-
 echo "**** Starting Ollama ****"
 ollama serve > /workspace/logs/ollama.log 2>&1 &  # Start Ollama and log output
 
--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONUNBUFFERED=1
+mkdir -p /workspace/logs /workspace/models
 
 #echo "**** Setting the timezone based on the TIME_ZONE environment variable. If not set, it defaults to Etc/UTC. ****" && \
 export TZ=${TIME_ZONE:-"Etc/UTC"} && \
